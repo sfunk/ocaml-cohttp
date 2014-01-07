770886d004ced4fca956982b05e2c38d159e474e
sfunk <sebastian.funk@gmail.com>
2014-01-07 16:08:45 +0100

    also added major and minor gcs

--- lib_test/test_net_event_alloc.ml
+++ lib_test/test_net_event_alloc.ml
@@ -4,14 +4,15 @@ open Cohttp_async
 
 let iterations = 30
 
-let alloc_array = Array.create ~len:iterations 0
-let time_array = Array.create ~len:iterations 0.
+let minor_alloc_array = Array.create ~len:iterations 0
 let major_alloc_array = Array.create ~len:iterations 0
+let minor_gcs = Array.create ~len:iterations 0
+let major_gcs = Array.create ~len:iterations 0
+let time_array = Array.create ~len:iterations 0.
 
 let rec make_req n = 
   let start_time = Time.now () in
-  let start_alloc = Gc.minor_words () in
-  let start_major_alloc = Gc.major_words () in
+  let gc1 = Gc.quick_stat () in
   let headers = Cohttp.Header.of_list ["connection","close"] in 
   let uri = Uri.of_string "http://www.google.com/" in
   Client.get ~headers uri >>= fun (res,body) ->
@@ -22,11 +23,12 @@ let rec make_req n =
   printf "\nResponse in bytes: %d\n" (String.length buf);
   let end_time = Time.now () in
   printf "Done in %s\n" (Time.diff end_time start_time |> Time.Span.to_string);
-  let end_alloc = Gc.minor_words () in
-  let end_major_alloc = Gc.major_words () in
-  alloc_array.(n) <- (end_alloc - start_alloc);
+  let gc2 = Gc.quick_stat () in
   time_array.(n) <- (Time.diff end_time start_time |> Time.Span.to_float);
-  major_alloc_array.(n) <- (end_major_alloc - start_major_alloc);
+  minor_alloc_array.(n) <- Float.to_int (gc2.Gc.Stat.minor_words -. gc1.Gc.Stat.minor_words);
+  major_alloc_array.(n) <- Float.to_int (gc2.Gc.Stat.major_words -. gc1.Gc.Stat.major_words);
+  minor_gcs.(n) <- (gc2.Gc.Stat.minor_collections - gc1.Gc.Stat.minor_collections);
+  major_gcs.(n) <- (gc2.Gc.Stat.major_collections - gc1.Gc.Stat.major_collections);
   if (n < iterations - 1) then make_req (n+1)
   else return ()
 
@@ -34,11 +36,19 @@ let rec make_req n =
 
 let _ = 
   let _ = Monitor.try_with ~extract_exn:true (fun () -> make_req 0) >>= (function
-    | Ok _ -> Printf.printf "M Allocations:\n%s\n"
+    | Ok _ -> 
+      Printf.printf "minor_heap size:%d MB\n"
+      (((Gc.Control.minor_heap_size (Gc.get ())) * (Sys.word_size) / 1_000_000));
+      Printf.printf "minor Allocations:\n%s\n"
+      (Array.to_list minor_alloc_array |> List.to_string  ~f:Int.to_string);
+      Printf.printf "major Allocations:\n%s\n"
       (Array.to_list major_alloc_array |> List.to_string  ~f:Int.to_string);
-      (* Printf.printf "Allocations:\n%s\n"
-      (Array.to_list alloc_array |> List.to_string  ~f:Int.to_string);
-      Printf.printf "Runtime:\n%s\n"
+      Printf.printf "minor GCs:\n%s\n"
+      (Array.to_list minor_gcs |> List.to_string  ~f:Int.to_string);
+      Printf.printf "major GCs:\n%s\n"
+      (Array.to_list major_gcs |> List.to_string  ~f:Int.to_string);
+
+(*      Printf.printf "Runtime:\n%s\n"
       (Array.to_list time_array |> List.to_string  ~f:(fun f -> sprintf "%.5f" (f*.1000.)) ); *)
       return (shutdown 0)
     | Error exn ->
